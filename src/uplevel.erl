9da99f52199dae69e00f696bd3304970b51c562f
Florian Odronitz <odo@mac.com>
2012-10-23 15:06:06 +0200

    removing encoding/decoding options. fixes #1.

--- src/uplevel.erl
+++ src/uplevel.erl
@@ -8,23 +8,22 @@
 
 -export([
 	handle/1, handle/2,
-    put/5,
-    put_command/3, put_command/4,
-	delete_command/2, delete_command/3,
-	get/4,
+    put/5, put/4,
+    put_command/3,
+	delete_command/2,
+	get/4, get/3,
     delete/3, delete/4,
     write/2, write/3,
-    range/4, range/5,
+    range/4,
     next/3,
     next_from_iterator/3,
     next_larger/3
 ]).
 
 -type table_handle() :: any().
--type put_options()  :: [{put_options, []} | {key_encoder, fun()}] | [].
+-type put_options()  :: [{put_options, []}] | [].
 -type get_options()  :: [{key_decoder, fun()}] | [].
 -type delete_options()  :: [{sync, boolean()}] | [].
--type keys_options()  :: [{key_encoder, fun()} | {key_decoder, fun()}] | [].
 -type max_key() :: binary() | function().
 -type command() :: {put, binary(), binary()} | {delete, binary()}.
 
@@ -41,25 +40,27 @@ handle(Path, Options) ->
 	{ok, Handle} = eleveldb:open(Path, Options),
 	Handle.
 
+-spec put(binary(), any(), any(), table_handle()) -> ok | {error, any()}.
+put(Bucket, Key, Value, Handle) ->
+    put(Bucket, Key, Value, Handle, []).
+
 -spec put(binary(), any(), any(), table_handle(), put_options()) -> ok | {error, any()}.
-put(Bucket, Key, Value, Handle, Options) ->
-    eleveldb:write(Handle, [put_command(Bucket, Key, Value, Options)], Options).
+put(Bucket, Key, Value, Handle, Options) when is_binary(Bucket) andalso is_binary(Key) ->
+    eleveldb:write(Handle, [put_command(Bucket, Key, Value)], Options).
 
 -spec put_command(binary(), any(), any()) -> {'put', binary(), binary()}.
-put_command(Bucket, Key, Value) ->
-    put_command(Bucket, Key, Value, []).
-
--spec put_command(binary(), any(), any(), put_options()) -> {'put', binary(), binary()}.
-put_command(Bucket, Key, Value, Options) ->
-	KeyEncoded =  encode_key(Key, 		 proplists:get_value(key_encoder, Options)),
-	KeyPrefixed = prefix_key(KeyEncoded, Bucket),
+put_command(Bucket, Key, Value) when is_binary(Bucket) andalso is_binary(Key) ->
+	KeyPrefixed = prefix_key(Key, Bucket),
     {put, KeyPrefixed, term_to_binary(Value)}.
 
+-spec get(binary(), any(), table_handle()) -> 'not_found' | {binary(), binary()}.
+get(Bucket, Key, Handle) ->
+    get(Bucket, Key, Handle, []).
+
 -spec get(binary(), any(), table_handle(), get_options()) -> 'not_found' | {binary(), binary()}.
-get(Bucket, Key, Handle, Options) ->
-	KeyEncoded =  encode_key(Key, 		 	proplists:get_value(key_encoder, Options)),
-	KeyPrefixed = prefix_key(KeyEncoded, 	Bucket),
-	case eleveldb:get(Handle, KeyPrefixed,  proplists:get_value(get_options, Options, [])) of
+get(Bucket, Key, Handle, Options) when is_binary(Bucket) andalso is_binary(Key) ->
+	KeyPrefixed = prefix_key(Key, Bucket),
+	case eleveldb:get(Handle, KeyPrefixed, proplists:get_value(get_options, Options, [])) of
 		{ok, Value} -> {Key, binary_to_term(Value)};
 		not_found -> not_found
 	end.
@@ -69,17 +70,12 @@ delete(Bucket, Key, Handle) ->
     delete(Bucket, Key, Handle, []).
 
 -spec delete(binary(), any(), table_handle(), delete_options()) -> ok.
-delete(Bucket, Key, Handle, Options) ->
-    eleveldb:write(Handle, [delete_command(Bucket, Key, Options)], Options).
+delete(Bucket, Key, Handle, Options) when is_binary(Bucket) andalso is_binary(Key) ->
+    eleveldb:write(Handle, [delete_command(Bucket, Key)], Options).
 
 -spec delete_command(binary(), any()) -> {'delete', binary()}.
-delete_command(Bucket, Key) -> 
-    delete_command(Bucket, Key, []).
-
--spec delete_command(binary(), any(), put_options()) -> {'delete', binary()}.
-delete_command(Bucket, Key, Options) -> 
-    KeyEncoded =  encode_key(Key,           proplists:get_value(key_encoder, Options)),
-    KeyPrefixed = prefix_key(KeyEncoded,    Bucket),
+delete_command(Bucket, Key) when is_binary(Bucket) andalso is_binary(Key) -> 
+    KeyPrefixed = prefix_key(Key, Bucket),
     {delete, KeyPrefixed}.
 
 -spec write([command()], any()) -> ok | {error, any()}.
@@ -91,50 +87,15 @@ write(Commands, Handle, Options) ->
     eleveldb:write(Handle, Commands, Options).
 
 -spec range(binary(), binary(), max_key(), table_handle()) -> [binary()].
-range(Bucket, Key, Max, Handle) ->
-    range(Bucket, Key, Max, Handle, []).
-
--spec range(binary(), binary(), max_key(), table_handle(), keys_options()) -> [binary()].
-range(Bucket, KeyMin, Max, Handle, Options) ->
-    % check if the encoding options make sense
-    {Encoder, Decoder} = {proplists:get_value(key_encoder, Options), proplists:get_value(key_decoder, Options)},
-    if
-        (Encoder =:= undefined) xor (Decoder =:= undefined) ->
-            throw({both_encoder_and_decoder_needed, [{key_encoder, Encoder}, {key_decoder, Decoder}]});
-        true ->
-            nothing
-    end,
+range(Bucket, Min, Max, Handle) when is_binary(Bucket) andalso is_binary(Min) andalso is_binary(Max) ->
     {ok, Iterator} = eleveldb:iterator(Handle, []),
-    Keys = 
-    case {Encoder, Decoder} of
-        {undefined, undefined} ->
-            KeyMinComposite = prefix_key(KeyMin, Bucket),
-            if
-                is_function(Max) ->
-                    case Max(KeyMin) of
-                        false -> [];
-                        true ->  next_key_fun(Iterator, Max, eleveldb:iterator_move(Iterator, KeyMinComposite), [])
-                    end;
-                is_binary(Max) andalso KeyMin > Max -> [];
-                is_binary(Max) -> next_key_max(Iterator, Max, eleveldb:iterator_move(Iterator, KeyMinComposite), []);
-                true -> throw({malformed_max_value, Max})
-            end;
-        {Encoder, Decoder} ->
-            KeyMinComposite = prefix_key(encode_key(KeyMin, Encoder), Bucket),
-            KeysEncodedValues =
-            if
-                is_function(Max) ->
-                    case Max(KeyMin) of
-                        false -> [];
-                        true ->  next_key_fun(Iterator, Max, eleveldb:iterator_move(Iterator, KeyMinComposite), [], Decoder)
-                    end;
-                true ->
-                    case encode_key(KeyMin, Encoder) > encode_key(Max, Encoder) of
-                        true  -> [];
-                        false -> next_key_max(Iterator, encode_key(Max, Encoder), eleveldb:iterator_move(Iterator, KeyMinComposite), [])
-                    end
-            end,
-            [{Decoder(K), V} || {K, V} <- KeysEncodedValues]
+    KeyMinComposite = prefix_key(Min, Bucket),
+    Keys =
+    case Min > Max of
+        true ->
+            [];
+        false ->
+            next_key_max(Iterator, Max, eleveldb:iterator_move(Iterator, KeyMinComposite), [])
     end,
     eleveldb:iterator_close(Iterator),
     lists:reverse(Keys).
@@ -192,39 +153,6 @@ next_from_iterator(Bucket, KeyMin, Iterator) ->
             not_found
     end.
 
-% get keys until the key equals a given key
-next_key_fun(Iterator, MaxFun, Candidate, KeysValues) ->
-    case Candidate of
-        {ok, CompositeKeyNext, ValueNext} ->
-            {_Bucket, Key} = expand_key(CompositeKeyNext),
-            case MaxFun(Key) of
-                true -> next_key_fun(Iterator, MaxFun, eleveldb:iterator_move(Iterator, next), [{Key, binary_to_term(ValueNext)} | KeysValues]);
-                false -> KeysValues
-            end;
-        {error, invalid_iterator}   -> KeysValues
-    end.
-
-% get keys until the key equals a given key
-next_key_fun(Iterator, MaxFun, Candidate, KeysValues, Decoder) ->
-    case Candidate of
-        {ok, CompositeKeyNext, ValueNext} ->
-            {_Bucket, Key} = expand_key(CompositeKeyNext),
-            case MaxFun(Decoder(Key)) of
-                true -> next_key_fun(Iterator, MaxFun, eleveldb:iterator_move(Iterator, next), [{Key, binary_to_term(ValueNext)} | KeysValues], Decoder);
-                false -> KeysValues
-            end;
-        {error, invalid_iterator}   -> KeysValues
-    end.
-
-encode_key(Key, undefined) when is_binary(Key) -> Key;
-encode_key(Key, undefined)					   -> key_not_binary(Key);
-encode_key(Key, Fun) ->
-	KeyEncoded = Fun(Key),
-	case is_binary(KeyEncoded) of
-		true  -> KeyEncoded;
-		false -> key_not_binary(Key)
-	end.
-
 prefix_key(Key, Prefix) ->
 	<<Prefix/binary, ?SEPARATOR/binary, Key/binary>>.
 
@@ -233,9 +161,6 @@ expand_key(BucketAndKey) ->
         [Bucket, Key]  -> {Bucket, Key};
         [BucketAndKey] -> {undefined, BucketAndKey}
     end.
-
-key_not_binary(Key) ->
-	throw({key_must_be_binary, Key}).
 
 %% ===================================================================
 %% EUnit tests
@@ -252,8 +177,6 @@ store_test_() ->
         {"put data with key encoding", fun test_put_encode/0},
       	{"put and delete data", fun test_delete/0},
         {"get range", fun test_range/0},
-        {"get range with encoding", fun test_range_with_encoding/0},
-        {"get range with fun", fun test_range_fun/0},
         {"get next key", fun test_next/0},
         {"get next key wirth itarator", fun test_next_from_iterator/0},
       	{"use commands", fun test_commands/0}
@@ -273,16 +196,6 @@ test_prefix_key()  ->
 test_put() ->
     Bucket = <<"bucket">>,
     Handle = handle(?TESTDB, [{create_if_missing, true}]),
-    ?assertException(
-    	throw,
-    	{key_must_be_binary, key},
-    	?MODULE:put(Bucket, key, value, Handle, [{put_options, [sync, true]}])
-    ),
-    ?assertException(
-    	throw,
-    	{key_must_be_binary, key},
-    	?MODULE:get(Bucket, key, Handle, [])
-    ),
     ?assertEqual(not_found, ?MODULE:get(Bucket, <<"nonexisting_key">>, Handle, [])),
     ?MODULE:put(Bucket, <<"key">>, value, Handle, [{put_options, [sync, true]}]),
     ?MODULE:put(<<"another_bucket">>, <<"another_key">>, value, Handle, [{put_options, [sync, true]}]),
@@ -292,19 +205,9 @@ test_put() ->
 test_put_encode() ->
     Bucket = <<"bucket">>,
     Handle = handle(?TESTDB, [{create_if_missing, true}]),
-    ?assertException(
-    	throw,
-    	{key_must_be_binary, min},
-    	?MODULE:range(Bucket, min, max, Handle, [{key_encoder, fun erlang:atom_to_list/1}, {key_decoder, fun erlang:list_to_atom/1}])
-    ),
-    ?assertException(
-    	throw,
-    	{key_must_be_binary, key},
-    	?MODULE:get(Bucket, key, Handle, [{key_encoder, fun(K) -> K end}])
-    ),
-    ?assertEqual(not_found, ?MODULE:get(Bucket, nonexisting_key, Handle, [{key_encoder, fun erlang:term_to_binary/1}])),
-    ?assertEqual(ok, ?MODULE:put(Bucket, key, value, Handle, [{key_encoder, fun erlang:term_to_binary/1}])),
-	?assertEqual({key, value}, ?MODULE:get(Bucket, key, Handle, [{key_encoder, fun erlang:term_to_binary/1}])).
+    ?assertEqual(not_found, ?MODULE:get(Bucket, <<"nonexisting_key">>, Handle)),
+    ?assertEqual(ok, ?MODULE:put(Bucket, <<"key">>, value, Handle)),
+	?assertEqual({<<"key">>, value}, ?MODULE:get(Bucket, <<"key">>, Handle)).
 
 test_delete() ->
     Bucket = <<"bucket">>,
@@ -328,44 +231,6 @@ test_range() ->
     ?assertEqual([], KeysEval(<<"x">>, <<"y">>)),
     ?assertEqual([], KeysEval(<<"">>, <<"">>)),
     ?assertEqual([], KeysEval(<<"a">>, <<"">>)).
-
-test_range_with_encoding() ->
-    Bucket = <<"bucket">>,
-    Handle = handle(?TESTDB, [{create_if_missing, true}]),
-    ?assertException(
-        throw,
-        {key_must_be_binary, min},
-        ?MODULE:range(Bucket, min, max, Handle, [{key_encoder, fun erlang:atom_to_list/1}, {key_decoder, fun erlang:list_to_atom/1}])
-    ),
-    Put = fun(Key, Value) -> ?MODULE:put(Bucket, Key, Value, Handle, [{put_options, [sync, true]}, {key_encoder, fun erlang:term_to_binary/1}]) end,
-    [Put(K, V) || {K, V} <- [{a, "a"}, {b, "b"}, {c, "c"}, {d, "d"}, {e, "e"}]],
-    EncoderDecoderOptions = [{key_encoder, fun erlang:term_to_binary/1}, {key_decoder, fun erlang:binary_to_term/1}],
-    KeysEval = fun(Min, Max) -> range(Bucket, Min, Max, Handle, EncoderDecoderOptions) end,
-    ?assertEqual([{a, "a"}], KeysEval(a, a)),
-    ?assertEqual([{a, "a"}], KeysEval('', a)),
-    ?assertEqual([{a, "a"}, {b, "b"}, {c, "c"}], KeysEval('', c)),
-    ?assertEqual([{c, "c"}, {d, "d"}, {e, "e"}], KeysEval(c, x)),
-    ?assertEqual([], KeysEval(x, y)),
-    ?assertEqual([], KeysEval('', '')),
-    ?assertEqual([], KeysEval(a, '')),
-    ?assertEqual([], KeysEval(a, fun(_) -> false end)),
-    ?assertEqual([{c, "c"}, {d, "d"}, {e, "e"}], KeysEval(c, fun(_) -> true end)),
-    ?assertEqual([{c, "c"}, {d, "d"}], KeysEval(c, fun(Key) -> Key < e end)),
-    ?assertEqual([], KeysEval(c, fun(Key) -> Key < a end)).
-
-
-test_range_fun() ->
-    Bucket = <<"bucket">>,
-    Handle = handle(?TESTDB, [{create_if_missing, true}]),
-    ?MODULE:put(Bucket, <<"a">>, a, Handle, [{put_options, [sync, true]}]),
-    ?MODULE:put(Bucket, <<"b">>, b, Handle, [{put_options, [sync, true]}]),
-    ?MODULE:put(Bucket, <<"c">>, c, Handle, [{put_options, [sync, true]}]),
-    ?MODULE:put(Bucket, <<"d">>, d, Handle, [{put_options, [sync, true]}]),
-    ?MODULE:put(Bucket, <<"e">>, e, Handle, [{put_options, [sync, true]}]),
-    ?assertEqual([], range(Bucket, <<"a">>, fun(_) -> false end, Handle)),
-    ?assertEqual([{<<"c">>, c}, {<<"d">>, d}, {<<"e">>, e}], range(Bucket, <<"c">>, fun(_) -> true end, Handle)),
-    ?assertEqual([{<<"c">>, c}, {<<"d">>, d}], range(Bucket, <<"c">>, fun(Key) -> Key < <<"e">> end, Handle)),
-    ?assertEqual([], range(Bucket, <<"c">>, fun(Key) -> Key < <<"a">> end, Handle)).
 
 test_next() ->
     Bucket1 = <<"bucket1">>,
